05851921c32666f9c96196e4fb461bd2e7b483af
Dhruv-kys <loltreat1@gmail.com>
2025-08-14 23:45:35 +0530

    Hyperparameter Training

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -18,23 +19,36 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
-    
-        for i in range(len(models)):
-            model = list(models.values())[i]
-            model.fit(X_train,y_train)
+        
+        for i, (name, model) in enumerate(models.items()):
+            # Get the parameter grid for this specific model
+            param_grid = params[name]  # params is a dict with model names as keys
             
+            gs = GridSearchCV(model, param_grid, cv=3)
+            gs.fit(X_train, y_train)
+            
+            # Set the best found parameters
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+            
+            # Predictions
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             
-            train_model_score = r2_score(y_train,y_train_pred)
-            test_model_score = r2_score(y_test,y_test_pred)
+            # Scores
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
             
-            report[list(models.keys())[i]] = test_model_score
-            
-            return report
+            # Store result
+            report[name] = test_model_score
+        
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
+
         
     except Exception as e:
         raise CustomException(e,sys)
